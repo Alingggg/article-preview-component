0f8fb85663ea961307df3109d8ce27e76334fc2e
Alingggg <lapitangeof2@gmail.com>
2024-10-10 17:23:52 +0800

    edit final version

---
+++ script.js
@@ -0,0 +1,17 @@
+const shareButton = document.querySelector(".article-preview__share-button-container");
+const shareButtonMobile = document.querySelector(".article-preview__share-icon--mobile");
+const shareContainer = document.querySelector(".article-preview__share-container");
+const shareIcon = document.querySelector(".article-preview__share-icon");
+
+shareButton.addEventListener("click", () => {
+    shareContainer.classList.toggle("hidden");
+    shareButton.classList.toggle("article-preview__share-button-container--dark");
+    shareIcon.classList.toggle("article-preview__share-icon--dark")
+    // if (window.innerWidth < 800) {
+    //     shareIcon.classList.toggle("article-preview__share-button-container--mobile");
+    // }
+}); 
+
+shareButtonMobile.addEventListener("click", e => {
+    shareContainer.classList.toggle("hidden");
+});
